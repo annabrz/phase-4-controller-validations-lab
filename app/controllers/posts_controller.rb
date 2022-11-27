6519424684abfcbc1597b92b68c18342b891db27
AnnaAyvazyan <ayvazyananna@yahoo.com>
2022-11-27 13:24:12 -0800

    commit tree bdb6c4794e2b737e33326c9939821e97b0a23f2c

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
@@ -11,7 +11,10 @@ class PostsController < ApplicationController
 
     post.update(post_params)
 
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
